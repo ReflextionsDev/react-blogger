7191d0c549a913e88d2096cecfb8c59bfd7dc828
ReflextionsDev <reflextionsdev@gmail.com>
2022-04-04 15:11:28 -0400

    react setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// Blogger
+//     Fetch all blogs from api
+//     Display all blogs using map
+//     Implement filtering by author
+//         Author list for select/options should be dynamically generated
+//     Implement edit blog text functionality
+//     Stretch: 
+//         Implement pagination (limit, page)
+//         Implement search
+
+// https://6239ddb128bcd99f02763cfe.mockapi.io/blogs
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
