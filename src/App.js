6826bc113f940faf6631bab4b53272bada887603
ReflextionsDev <reflextionsdev@gmail.com>
2022-04-04 17:15:28 -0400

    get blog api and state update

--- src/App.js
+++ src/App.js
@@ -8,16 +8,84 @@
 //         Implement pagination (limit, page)
 //         Implement search
 
-// https://6239ddb128bcd99f02763cfe.mockapi.io/blogs
+// Switch to didmount
 
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+const blogURL = 'https://6239ddb128bcd99f02763cfe.mockapi.io/blogs'
+// const blogData = await fetchblogData
+
+const fetchblogData = async () => {
+  const response = await fetch(blogURL,
+    {
+      method: "GET"
+    })
+
+  const responseJSON = await response.json()
+  return responseJSON
 }
 
-export default App;
+export class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      blogs: [
+        {
+          author: "Darren Abbott",
+          createdAt: "2022-03-22T10:36:37.176Z",
+          id: "1",
+          text: "Iusto et in et. Nulla accusantium fugit. Et qui dolorem inventore soluta et veritatis. Aut ut aut non laudantium eveniet suscipit odit. Sapiente sint nihil nihil sit et molestias. In nisi omnis quas et sed aut minus aperiam ea.\n \rLaudantium quo quisquam quae. Et et quas officia perspiciatis iusto sunt sunt eaque. Quidem sit voluptas deserunt sequi magni.\n \rEst est facere cumque ipsam omnis animi. Voluptatem magnam officiis architecto possimus. Quia similique aut eos qui. Quasi quae sed aliquam.",
+          title: "dicta",
+        }
+      ]
+    }
+  }
+
+  // componentDidMount
+
+  getBlog = async () => {
+    const data = await fetchblogData()
+    console.log(data)
+    this.setState({ blogs: data })
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h1>React Blogger</h1>
+        <button
+          name='fetchButton'
+          onClick={this.getBlog}>
+          Get Blogs
+        </button>
+
+        <div className='blogs'>
+          {this.state.blogs.map((post, idx) => {
+            return <Blog
+              key={idx}
+              author={post.author}
+              createdAt={post.createdAt}
+              id={post.id}
+              text={post.text}
+              title={post.title}
+            />
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+export default App
+
+// Blog Component
+function Blog(props) {
+
+  return <div className='blog'>
+    <h2>{props.title}</h2>
+    <h3>Posted By: {props.author}</h3>
+    <h3>Posted On: {props.createdAt}</h3>
+    <p>{props.text}</p>
+  </div>
+}
