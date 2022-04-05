3fee6e38560ea3082c5bc257874a08180581ba88
ReflextionsDev <reflextionsdev@gmail.com>
2022-04-05 15:44:09 -0400

    comment out placeholder state data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-// Blogger
-//     Fetch all blogs from api
-//     Display all blogs using map
-//     Implement filtering by author
-//         Author list for select/options should be dynamically generated
-//     Implement edit blog text functionality
-//     Stretch: 
-//         Implement pagination (limit, page)
-//         Implement search
-
-// Switch to didmount
-
+// Imports
 import './App.css';
 import React, { Component } from 'react'
 import Blog from './component/Blog';
 
+// API interactions
 const blogURL = 'https://6239ddb128bcd99f02763cfe.mockapi.io/blogs'
-// const blogData = await fetchblogData
-
 const fetchblogData = async () => {
   const response = await fetch(`${blogURL}`)
-
   const responseJSON = await response.json()
   return responseJSON
 }
@@ -29,31 +16,34 @@ export class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: [
-        {
-          author: "Darren Abbott",
-          createdAt: "2022-03-22T10:36:37.176Z",
-          id: "1",
-          text: "Iusto et in et. Nulla accusantium fugit. Et qui dolorem inventore soluta et veritatis. Aut ut aut non laudantium eveniet suscipit odit. Sapiente sint nihil nihil sit et molestias. In nisi omnis quas et sed aut minus aperiam ea.\n \rLaudantium quo quisquam quae. Et et quas officia perspiciatis iusto sunt sunt eaque. Quidem sit voluptas deserunt sequi magni.\n \rEst est facere cumque ipsam omnis animi. Voluptatem magnam officiis architecto possimus. Quia similique aut eos qui. Quasi quae sed aliquam.",
-          title: "dicta",
-        }
-      ],
+      // blogs: [
+      //   {
+      //     author: "Darren Abbott",
+      //     createdAt: "2022-03-22T10:36:37.176Z",
+      //     id: "1",
+      //     text: "Iusto et in et. Nulla accusantium fugit. Et qui dolorem inventore soluta et veritatis. Aut ut aut non laudantium eveniet suscipit odit. Sapiente sint nihil nihil sit et molestias. In nisi omnis quas et sed aut minus aperiam ea.\n \rLaudantium quo quisquam quae. Et et quas officia perspiciatis iusto sunt sunt eaque. Quidem sit voluptas deserunt sequi magni.\n \rEst est facere cumque ipsam omnis animi. Voluptatem magnam officiis architecto possimus. Quia similique aut eos qui. Quasi quae sed aliquam.",
+      //     title: "dicta",
+      //   }
+      // ],
       selectedAuthor: "All"
     }
   }
 
+  // Load blogs from API on mount
   componentDidMount = async () => {
     const data = await fetchblogData()
     // console.log(data)
     this.setState({ blogs: data })
   }
 
+  // Filter blog posts by selected author
   handleCategorySelect = (e) => {
     this.setState({
       selectedAuthor: e.target.value,
     });
   };
 
+  // Update blog text as filled
   editBlogPost = (e, id) => {
     const newData = { ...this.state }
     newData.blogs[id - 1].text = e.target.value
@@ -93,4 +83,5 @@ export class App extends Component {
     )
   }
 }
+
 export default App
